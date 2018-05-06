2382b4c91f6d607d737081c1006d2e3145103086
Szeto Bo <szetobo@gmail.com>
2018-05-06 15:36:40 +0800

    Step 2: add chance score implementation (green)

--- lib/yatzy.rb
+++ lib/yatzy.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class Yatzy
+  attr_reader :dices
+
+  def initialize(dices)
+    @dices = dices.split('-').map(&:to_i)
+  end
+
+  def chance
+    dices.sum
+  end
 end
