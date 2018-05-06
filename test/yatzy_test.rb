b6d518e82a8fd305e46275dc112ded1e9d4bf058
Szeto Bo <szetobo@gmail.com>
2018-05-06 15:43:57 +0800

    Step 3: add ones score test (red)

--- test/yatzy_test.rb
+++ test/yatzy_test.rb
@@ -11,4 +11,10 @@ describe Yatzy do
     assert_equal 14, Yatzy.new('1-1-3-3-6').chance
     assert_equal 21, Yatzy.new('1-4-5-5-6').chance
   end
+
+  it 'should calculuate ones score' do
+    assert_equal 2, Yatzy.new('1-1-2-4-4').ones
+    assert_equal 1, Yatzy.new('1-2-2-3-5').ones
+    assert_equal 0, Yatzy.new('3-3-3-4-5').ones
+  end
 end
