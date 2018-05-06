b866d0922cf0625bb6ca49e8e76b675db39b97b1
Szeto Bo <szetobo@gmail.com>
2018-05-06 15:33:39 +0800

    Step 1: add chance score test (red)

--- test/yatzy_test.rb
+++ test/yatzy_test.rb
@@ -6,4 +6,9 @@ require 'minitest/pride'
 require_relative '../lib/yatzy'
 
 describe Yatzy do
+  it 'should calculuate chance score' do
+    assert_equal 15, Yatzy.new('1-2-3-4-5').chance
+    assert_equal 14, Yatzy.new('1-1-3-3-6').chance
+    assert_equal 21, Yatzy.new('1-4-5-5-6').chance
+  end
 end
