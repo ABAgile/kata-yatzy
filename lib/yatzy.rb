b3fda93582361605435b741b092765b4a0bd4f70
Szeto Bo <szetobo@gmail.com>
2018-05-06 17:25:58 +0800

    Step 18: add four_of_a_kind implementation (green)

--- lib/yatzy.rb
+++ lib/yatzy.rb
@@ -56,4 +56,15 @@ class Yatzy
       return 3 * face if dices.count { |dice| dice == face } >= 3
     end
   end
+
+  def four_of_a_kind
+    faces = dices.uniq
+    return 0 if faces.size > 2
+
+    faces.each do |face|
+      return 4 * face if dices.count { |dice| dice == face } >= 4
+    end
+
+    0
+  end
 end
