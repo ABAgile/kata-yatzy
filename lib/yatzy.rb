cc242a0869086d94ed8be94c6f6a77c3fe71041b
bootleq <bootleq@gmail.com>
2018-05-09 17:42:13 +0800

    Round 1

--- lib/yatzy.rb
+++ lib/yatzy.rb
@@ -1,4 +1,52 @@
 # frozen_string_literal: true
 
 class Yatzy
+  # attr_accessor :sum_score
+
+  # def initialize(dice)
+  #   dice = dice
+  # end
+
+  def self.score(dice)
+    dice.sum
+  end
+
+  def self.yatzy(dice)
+    return 50 if dice.uniq.size == 1
+  end
+
+  def self.ones(dice)
+    return dice.count(1)
+  end
+
+  def self.twos(dice)
+    return dice.count(2) * 2
+  end
+
+  def self.threes(dice)
+    return dice.count(3) * 3
+  end
+
+  def self.fours(dice)
+    return dice.count(4) * 4
+  end
+
+  def self.fives(dice)
+    return dice.count(5) * 5
+  end
+
+  def self.sixes(dice)
+    return dice.count(6) * 6
+  end
+
+  def self.pair(dice)
+    (1..6).map do |num|
+      if dice.count(num) >= 2
+        num * 2
+      else
+        0
+      end
+    end.sum
+  end
+
 end
--- test/yatzy_test.rb
+++ test/yatzy_test.rb
@@ -6,4 +6,57 @@ require 'minitest/pride'
 require_relative '../lib/yatzy'
 
 describe Yatzy do
+  # let(:subject) { Yatzy }
+  # let(:default_dice) { [1,2,3,4,6] }
+
+  it 'should calculate correct chance' do
+    # dice = default_dice
+    dice = [1,2,3,4,6]
+
+    # sum = subject.new(dice).score
+
+    assert_equal 16, Yatzy.score(dice)
+  end
+
+  it 'yatzy' do
+    dice = [1, 1, 1, 1, 1]
+
+    assert_equal 50, Yatzy.yatzy(dice)
+  end
+
+  it 'ones' do
+    dice = [1, 1, 2, 2, 2]
+    assert_equal 2, Yatzy.ones(dice)
+
+    dice = [1, 1, 1, 1, 2]
+    assert_equal 4, Yatzy.ones(dice)
+  end
+
+  it 'twos' do
+    dice = [1, 1, 2, 2, 2]
+    assert_equal 6, Yatzy.twos(dice)
+  end
+
+  it 'threes' do
+    dice = [1, 1, 3, 3, 3]
+    assert_equal 9, Yatzy.threes(dice)
+  end
+
+  it 'fours' do
+    dice = [1, 1, 4, 4, 3]
+    assert_equal 8, Yatzy.fours(dice)
+  end
+
+  it 'fives' do
+    dice = [5, 5, 4, 4, 3]
+    assert_equal 10, Yatzy.fives(dice)
+  end
+  it 'sixes' do
+    dice = [6, 6, 4, 4, 3]
+    assert_equal 12, Yatzy.sixes(dice)
+  end
+  it 'pair' do
+    dice = [3, 3, 3, 4, 4]
+    assert_equal 14, Yatzy.pair(dice)
+  end
 end
