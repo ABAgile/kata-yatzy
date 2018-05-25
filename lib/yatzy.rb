8a2e78a040bc45d19fb56dc8b381a3cc91e66ef2
John <johnwu2613@gmail.com>
2018-05-26 07:51:07 +0800

    Initialize
    1. add initialize method
    2. add earn points method and test

--- lib/yatzy.rb
+++ lib/yatzy.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class Yatzy
+  attr_accessor :dice
+
+  def initialize(dice)
+    @dice = dice
+  end
+
+  def earn(num)
+    dice.sum{ |d| d if d == num }
+  end
 end
--- test/yatzy_test.rb
+++ test/yatzy_test.rb
@@ -6,4 +6,10 @@ require 'minitest/pride'
 require_relative '../lib/yatzy'
 
 describe Yatzy do
+  let(:dice) { [1,1,1,1,1] }
+
+  it 'earn points' do
+
+    assert_equal Yatzy.new(dice).earn(1), 5
+  end
 end
