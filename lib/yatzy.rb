46a54e2cc3d607c7247d156655e90e4971368276
Azi Chen <azi.chen@abagile.com>
2018-05-25 16:19:18 +0800

    Pair and n_of_a_kind can use same formula

--- lib/yatzy.rb
+++ lib/yatzy.rb
@@ -14,26 +14,15 @@ class Yatzy
   end
 
   def self.pair(dice)
-    result = group(dice).select{|k, v| v == 2}.sort_by{|k, v| k }.to_h
-    (result.size == 0) ? 0 : result.sum{|k, _| k * 2}
-  end
-
-  def self.group(dice)
-    # {2 => 4, 1 => 1}
-    Hash[dice.group_by{|k| k}.map{|k, v| [k, v.size]}]
-  end
-
-  def self.n_of_a_kind(dice, n)
-    result = group(dice).select{ |k, v| v == n }.sort_by{ |k, v| k }.to_h
-    (result.size == 0) ? 0 : result.sum{|k, _| k * n}
+    n_of_sum(dice, 2)
   end
 
   def self.three_of_a_kind(dice)
-    n_of_a_kind(dice, 3)
+    n_of_sum(dice, 3)
   end
 
   def self.four_of_a_kind(dice)
-    n_of_a_kind(dice, 4)
+    n_of_sum(dice, 4)
   end
 
   def self.small_straight(dice)
@@ -46,5 +35,17 @@ class Yatzy
 
   def self.full_house(dice)
     group(dice).values.sort == [2, 3] ? dice.sum : 0
+  end
+
+  private
+
+  def self.group(dice)
+    # {2 => 4, 1 => 1}
+    Hash[dice.group_by{|k| k}.map{|k, v| [k, v.size]}]
+  end
+
+  def self.n_of_sum(dice, n)
+    result = group(dice).select{ |k, v| v == n }.sort_by{ |k, v| k }.to_h
+    (result.size == 0) ? 0 : result.sum{|k, _| k * n}
   end
 end
