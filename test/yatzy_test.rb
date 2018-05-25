a84642e7b7aca7d122d5e16412f0ab07711ce910
Azi Chen <azi.chen@abagile.com>
2018-05-25 10:55:41 +0800

    Simplify count test

--- test/yatzy_test.rb
+++ test/yatzy_test.rb
@@ -25,27 +25,19 @@ describe Yatzy do
   end
 
   it 'count' do
-    pattern = -> (count_type, num) do
+    pattern = -> (count_type) do
       case count_type
-      when 1 then [num, 2,   3,   4,   5]
-      when 2 then [num, num, 3,   4,   5]
-      when 3 then [num, num, num, 4,   5]
-      when 4 then [num, num, num, num, 5]
-      when 5 then [num, num, num, num, num]
+      when 1 then [1, 2, 3, 4, 5]
+      when 2 then [1, 1, 3, 4, 5]
+      when 3 then [1, 1, 1, 4, 5]
+      when 4 then [1, 1, 1, 1, 5]
+      when 5 then [1, 1, 1, 1, 1]
       end
     end
 
-    [
-      [[1, 6],             1], # 1*1, 6*1
-      [[1, 2, 6],          2], # 1*2, 2*2, 6*2
-      [[1, 2, 3, 6],       3], # 1*3, 2*3, 3*3, 6*3
-      [[1, 2, 3, 4, 6],    4], # 1*4, 2*4, 3*4, 4*4, 6*4
-      [[1, 2, 3, 4, 5, 6], 5]  # 1*5, 2*5, 3*5, 4*5, 5*5
-    ].each do |replace_vars, count_type|
-      replace_vars.each do |replace_val|
-        dice = pattern.call(count_type, replace_val)
-        assert_equal count_type * replace_val, Yatzy.count(dice, replace_val)
-      end
+    (1..5).each do |count_type|
+      dice = pattern.call(count_type)
+      assert_equal count_type * 1, Yatzy.count(dice, 1)
     end
   end
 
