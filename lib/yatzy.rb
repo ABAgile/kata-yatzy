a30fda4e5e97c072b4e2ab2f68a931f010a530aa
John <johnwu2613@gmail.com>
2018-05-26 11:06:09 +0800

    Add dice group and get_score
    1. add dice group to caluculate all dice size
    2. add yatzy function

--- lib/yatzy.rb
+++ lib/yatzy.rb
@@ -1,13 +1,38 @@
 # frozen_string_literal: true
 
 class Yatzy
-  attr_accessor :dice
+  attr_accessor :dice, :group
 
   def initialize(dice)
-    @dice = dice
+    @dice  = dice
+    @group = dice_group(dice)
   end
 
   def earn(num)
-    dice.sum{ |d| d if d == num }
+    dice.sum{ |d| d if d == num }.to_i
+  end
+
+  def dice_size(num)
+    dice.count{ |d| d if d == num }.to_i
+  end
+
+  def dice_group(dice)
+    (1..6).each_with_object({}) do |num, hash|
+      hash[num] = dice_size(num)
+    end
+  end
+
+  def get_score
+    score = case
+            when yatzy?
+              50
+            end
+    score
+  end
+
+  private
+
+  def yatzy?
+    group.values.any?(5)
   end
 end
--- test/yatzy_test.rb
+++ test/yatzy_test.rb
@@ -9,7 +9,10 @@ describe Yatzy do
   let(:dice) { [1,1,1,1,1] }
 
   it 'earn points' do
-
     assert_equal Yatzy.new(dice).earn(1), 5
+  end
+
+  it 'Yatzy' do
+    assert_equal Yatzy.new(dice).get_score, 50
   end
 end
